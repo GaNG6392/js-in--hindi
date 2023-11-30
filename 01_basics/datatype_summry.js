77da64199e22c5c2a5b12e96de71bbf32658c36f
Ganga Ram Nishad <105135809+GaNG6392@users.noreply.github.com>
2023-11-30 06:31:43 +0000

    datatype summry

---
+++ 01_basics/datatype_summry.js
@@ -0,0 +1,18 @@
+//++++++++++++++++++++data type summery  ++++++++++++++
+
+//Stack(Primitive)    Heap(Non Primitive)
+
+let myYoutubeName = "RaMM Tutorial"
+let anotherName = myYoutubeName
+anotherName ="chaiaurcode"
+console.log(anotherName)
+console.log(myYoutubeName)
+
+let user1 ={
+    id:123,
+    email:"user@gmail.com"
+}
+let user2 = user1
+user2.id = 123344,
+user1.email = ("user2@gmail.com")
+console.log(user1.id)
