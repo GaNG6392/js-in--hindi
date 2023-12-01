c4752eb2a1a422e85f6a061975ffbe4265e52339
Ganga Ram Nishad <105135809+GaNG6392@users.noreply.github.com>
2023-12-01 19:52:42 +0000

    learn about json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,4 +20,42 @@ const regularUser = {
         }
     }
 }
-console.log(regularUser.fullName.userFullName.firstName);
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = {
+    1:"a",
+    2:"b"
+}
+const obj2 = {
+    3:"c",
+    4:"d"
+}
+const obj4 = {
+    5:"e",
+    6:"f"
+}
+const obj3 = {...obj1,...obj2}
+// console.log(Object.assign(obj1,obj2,obj4));
+// console.log(obj3)
+const user =[
+    {
+        id:1,
+        email:"gr@gmail.com"
+    },
+    {
+        id:2,
+        email:"grn@gmail.com"
+    },
+    {
+        id:3,
+        email:"grn2@gmail.com"
+    }
+]
+ 
+// 1  ---const data = {...obj1,...obj2}
+// 2  ---const data = Object.assign(obj1,obj2,obj4)
+// 3  ---const data= Object.keys(tinderUser)
+// 4  ---const data= Object.values(tinderUser)
+// 5  ---const data= Object.entries(tinderUser)
+// 6  ---const data= tinderUser.hasOwnProperty("isLoged")
+console.log(data)
