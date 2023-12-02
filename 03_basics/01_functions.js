16bddc5299ebdc8c4cc58bf0dc035250d59f2a70
Ganga Ram Nishad <105135809+GaNG6392@users.noreply.github.com>
2023-12-02 19:53:05 +0000

    create function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -2,7 +2,7 @@ function sumTowNumber(a, b) {
   //create function with parameter and argument
   return a + b;
 }
-console.log(sumTowNumber(21, 42));
+// console.log(sumTowNumber(21, 42));
 
 const userData = function () {
   // We can store function in variable
@@ -14,4 +14,43 @@ const userData = function () {
   };
   console.log(user);
 };
-userData();
+// userData();
+
+function addCartPrice(...num1) {
+  //(... ) is called rest oparater
+  return num1.reduce((acc, crt) => acc + crt);
+}
+console.log(addCartPrice(200, 300, 500));
+
+const user = {
+  userName: 'ganga',
+  price: 199,
+};
+function handleObject(anyObject) {
+  console.log(
+    `${anyObject.userName} braught a watch that price is ${anyObject.price} Rs.`
+  );
+}
+// handleObject(user);
+handleObject({
+  userName: 'ganga ram',
+  price: 499,
+});
+
+const array1 = [100, 300, 500];
+
+function handleArray(anyArray) {
+  return anyArray[2];
+}
+// console.log(handleArray(array1));
+console.log(handleArray([100, 300, 500]));
+
+console.log(one(5));
+function one(num) {
+  return num + 1;
+}
+
+const two = function (num) {
+  return num + 6;
+};
+console.log(two(34));
