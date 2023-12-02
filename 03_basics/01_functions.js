30cf68d9113ca7626185413e2a6a4d52a1780243
Ganga Ram Nishad <105135809+GaNG6392@users.noreply.github.com>
2023-12-02 07:30:16 +0000

    create function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,17 @@
+function sumTowNumber(a, b) {
+  //create function with parameter and argument
+  return a + b;
+}
+console.log(sumTowNumber(21, 42));
+
+const userData = function () {
+  // We can store function in variable
+  const user = {
+    id: 1,
+    name: 'gr',
+    email: 'gr@gmail.com',
+    password: '123abc',
+  };
+  console.log(user);
+};
+userData();
